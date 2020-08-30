4dc652cef0c194580cbe16ae92bee27955647a67
weblinggau <adjiersandr@gmail.com>
2020-08-30 20:58:26 +0700

    memperbaharui source code cek.php

--- example/cek.php
+++ example/cek.php
@@ -1,15 +1,51 @@
 <?php 
 session_start();
-require 'pak.php';
+require './pak.php';
 
 $npm = $_POST['npm'];
 $pass = $_POST['password'];
+if (isset($_POST['jts'])) {
+	$_SESSION['atribut'] = 'alert-success';
+	$_SESSION['pesan'] = 'Data berhasil tersimpan, pendaftaran berhasil';
+	header("location:index.php");
+}
 
 $cek = dataPortal($npm,$pass);
 
 if (empty($cek)) {
-	
-}
-
-
+	$_SESSION['atribut'] = 'alert-warning';
+	$_SESSION['pesan'] = 'NPM dan Password salah juga mungkin gangguan lain.!';
+	header("location:index.php");
+}else{
  ?>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>register</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+</head>
+
+<body>
+    <div class="login-clean">
+
+        <form method="post">
+        	<input type="hidden" name="jts" value="add">
+            <div class="form-group"><input class="form-control" type="text" name="npm" placeholder="NPM" value="<?= $cek['npm']; ?>"></div>
+            <div class="form-group"><input class="form-control" type="text" name="nama" placeholder="Nama" value="<?= $cek['nama']; ?>"></div>
+            <div class="form-group"><input class="form-control" type="text" name="TTL" placeholder="ttl" value="<?= $cek['ttl']; ?>"></div>
+            <div class="form-group"><input class="form-control" type="text" name="prodi" placeholder="prodi" value="<?= $cek['prodi']; ?>"></div>
+            <div class="form-group"><input class="form-control" type="text" name="agama" placeholder="agama" value="<?= $cek['agama']; ?>"></div>
+            <div class="form-group"><button class="btn btn-primary btn-block" type="submit">REGISTER</button></div>
+        </form>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
+<?php } ?>
